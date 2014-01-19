724cddb87e4d17b9542f317fdcbf1671ca2751db
Rainer Schuster <rainer.schuster@alere.com>
2014-01-19 02:02:04 +0100

    Added UI prototype

--- slotty/app.py
+++ slotty/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin
 import gevent
 
 import random
@@ -12,48 +13,48 @@ app = Flask(__name__)
 app.config.from_object('slotty.config')
 app.config.from_envvar('SLOTTY_CONFIG_FILE')
 
+fastest_laps = { i: 999.0 for i in range(1,7)}
+laps = { i: 0 for i in range(1,7)}
 
 def read():
-    #TODO: add the datalogger data here
+    global fastest_laps
+    global laps
 
     lap_time_estimate = 6.500
     variation = 0.500
     lap_time = random.uniform(lap_time_estimate,
                               lap_time_estimate + 0.100)
-    data = []
-    for controller in range(6):
-        data.append({'lapTime': lap_time,
-                     'controller': controller})
+    data_list = []
+    for controller in range(1,7):
+        laps[controller] += 1
+        fastest = fastest_laps[controller]
+        data = {'lapTime': lap_time,
+                'controller': controller,
+                'lapTimeFastest': fastest,
+                'laps': laps[controller]}
         offset = random.uniform(-variation, variation)
+        if lap_time < fastest:
+            data["lapTimeFastest"] = lap_time
+            fastest_laps[controller] = lap_time
+        data_list.append(data)
         lap_time += offset
 
     gevent.sleep(lap_time)
-    for i in sorted(data, key=itemgetter('lapTime')):
+    for i in sorted(data_list, key=itemgetter('lapTime')):
         gevent.sleep(i['lapTime'] - lap_time)
         yield i
 
 
-class SlottyNamespace(BaseNamespace):
-    sockets = {}
+class SlottyNamespace(BaseNamespace, BroadcastMixin):
     sensing = False
 
     def recv_connect(self):
         app.logger.info("Client Connected")
-        self.sockets[id(self)] = self
         self.loop_sensors()
 
     def disconnect(self, *args, **kwargs):
-        if id(self) in self.sockets:
-            app.logger.info("Client %s Disconnected", id(self))
-            del self.sockets[id(self)]
+        app.logger.info("Client Disconnected", id(self))
 
-    # broadcast to all sockets on this channel!
-    @classmethod
-    def broadcast(self, event, message):
-        for ws in self.sockets.values():
-            ws.emit(event, message)
-
-    @classmethod
     def loop_sensors(self):
         if self.sensing:
             return
@@ -61,7 +62,7 @@ class SlottyNamespace(BaseNamespace):
         while True:
             for race_data in read():
                 app.logger.debug(race_data)
-                self.broadcast("racetime", race_data)
+                self.broadcast_event("racetime", race_data)
 
 
 @app.route('/socket.io/<path:rest>')
